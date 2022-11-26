7821b1aa933f6c0d4aa9e39e57812a0bf40c12b1
Alex <aabadp02@estudiantes.unileon.es>
2022-11-26 16:34:00 +0100

    Commit inicial del proyecto backend de declivia

---
+++ src/main/java/com/decliviaprospring/decliviaprospring/DecliviaproSpringApplication.java
@@ -0,0 +1,13 @@
+package com.decliviaprospring.decliviaprospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecliviaproSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecliviaproSpringApplication.class, args);
+	}
+
+}
